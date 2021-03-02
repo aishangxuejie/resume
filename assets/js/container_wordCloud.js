f472e32d919cd1ce4fe8a039106d356365a4d37b
aishangxuejie <cuiguiming@outlook.com>
2021-03-02 22:02:32 +0800

    update wordcloud and contents

--- assets/js/container_wordCloud.js
+++ assets/js/container_wordCloud.js
@@ -6,7 +6,7 @@ var wordCloud_option = {
             type: 'wordCloud',
             // drawOutOfBound:true,
             gridSize: 1,
-            sizeRange: [12, 55],
+            sizeRange: [8, 35],
             rotationRange: [-45, 0, 45, 90],
 
             //这是让词云图的颜色随机
@@ -26,7 +26,7 @@ var wordCloud_option = {
             height: 200,
             right: null,
             bottom: null,
-            data:  [{'name':'java','value':9},{'name':'python','value':9},{'name':'vue','value':9},{'name':'Linux','value':9},]
+            data: [{'name':'java','value':9},{'name':'Spring','value':9},{'name':'python','value':6},{'name':'vue','value':7},{'name':'Linux','value':7},{'name':'SpringBoot','value':7},{'name':'DataV','value':7},{'name':'Oracle','value':7},{'name':'Redis','value':7},{'name':'MySql','value':9},{'name':'nosql','value':6},{'name':'JVM','value':5},{'name':'Mybatis','value':7},{'name':'Git','value':5},{'name':'面向对象','value':5},{'name':'多线程','value':5},{'并发':'Git','value':5},]
         }]
 }
 // wordCloud_option.series[0].data=ddd;
